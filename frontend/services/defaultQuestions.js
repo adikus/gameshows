ef35e0ec68e9ad8ebab74b9a6ecf36049391256e
Andrej Hoos <andrej.hoos@gmail.com>
2023-09-24 23:59:44 +0200

    Add IDs to default questions

--- frontend/services/defaultQuestions.js
+++ frontend/services/defaultQuestions.js
@@ -3,25 +3,25 @@ export default {
         {
             name: 'Category 1',
             questions: [
-                { question: 'Question 1', answer: 'Answer 1', value: 100 },
-                { question: 'Question 2', answer: 'Answer 2', value: 200 },
-                { question: 'Question 3', answer: 'Answer 3', value: 300 }
+                { id: 'W02ygdk1', question: 'Question 1', answer: 'Answer 1', value: 100 },
+                { id: 'EWj13j5O', question: 'Question 2', answer: 'Answer 2', value: 200 },
+                { id: 'JgB2i8Hl', question: 'Question 3', answer: 'Answer 3', value: 300 }
             ]
         },
         {
             name: 'Category 2',
             questions: [
-                { question: 'Question 4', answer: 'Answer 4', value: 100 },
-                { question: 'Question 5', answer: 'Answer 5', value: 200 },
-                { question: 'Question 6', answer: 'Answer 6', value: 300 }
+                { id: 'F1s9doJI', question: 'Question 4', answer: 'Answer 4', value: 100 },
+                { id: 'QZd4sd6N', question: 'Question 5', answer: 'Answer 5', value: 200 },
+                { id: 'p9hGl6Ux', question: 'Question 6', answer: 'Answer 6', value: 300 }
             ]
         },
         {
             name: 'Category 3',
             questions: [
-                { question: 'Question 7', answer: 'Answer 7', value: 100 },
-                { question: 'Question 8', answer: 'Answer 8', value: 200 },
-                { question: 'Question 9', answer: 'Answer 9', value: 300 }
+                { id: 'VLlZjK8g', question: 'Question 7', answer: 'Answer 7', value: 100 },
+                { id: '7Ys52Wrz', question: 'Question 8', answer: 'Answer 8', value: 200 },
+                { id: 'ooAgcnwx', question: 'Question 9', answer: 'Answer 9', value: 300 }
             ]
         }
     ]
